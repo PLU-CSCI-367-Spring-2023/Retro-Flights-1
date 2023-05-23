3bb313089fd9d293a1006f66a04dd2f96bc3e286
AbelWondimuL <abelwondimu208@gmail.com>
2023-05-23 03:12:33 +0000

    Changed to CSV file

--- .devcontainer/pg_init/02_insert_rows.sql
+++ .devcontainer/pg_init/02_insert_rows.sql
@@ -1,5 +1,5 @@
-copy airports( Airport_ID, Name, City, Country, IATA, ICAO, Latitude, Longitude, Altitude, Timezone, DST, Tz_database_Timezone, Type, Source)
-from '/docker-entrypoint-initdb.d/seed_data/airports.dat'
+copy airports( airport_ID, name, city, country, iata, icao, latitude, longitude, altitude, timezone, dst, tz_database_timezone, type, source)
+from '/docker-entrypoint-initdb.d/seed_data/airports.csv'
 delimiter ',' 
 csv header;
     
